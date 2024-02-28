fcf7fac314f415cbaf35377b5819c31618d69b4c
yakson30 <adugayakubu@gmail.com>
2024-02-28 10:10:52 +0100

    started python with mr emmanuel

---
+++ class1.py
@@ -0,0 +1,71 @@
+print("hello world")
+
+# Vairables in python 
+# PneakesCase
+# PasscalCase
+# CameCase
+# First_Name = 'emannuel' SNAKE CASE
+# last Name = 'forest ' SNAKE CASE
+# NewAge = 23  'PASCAL CASE'
+First_Name = 'Oyencahi'
+print(First_Name)
+
+
+
+# case function in Python
+print(First_Name.upper())
+print(First_Name.lower())
+print(First_Name.title())
+print(First_Name.capitalize())
+
+
+# datatypes in python
+# strings
+# intergers
+# float
+# set
+# list
+# tuple
+# dict
+# complex
+# boolean
+
+setOfNumbers = {1, 2, 3}
+print(setOfNumbers)
+
+Numbers = [1, 2, 3 , 4, 5, 6, 7]
+
+MyTuple = ('Dangote', 'Patricia',  'darey, abegapp') 
+Dictionary = {'Name': 'Dangote', 'Type': 'Business'}
+MyComplex = 10+1j
+hammantan = True
+print(MyTuple)
+print(len(MyTuple))
+
+""" this is another comment"""
+'''this is something wired'''
+
+# string new topic
+first_text = 'this is some content'
+second_text = 'this is another content'
+
+# concatation
+newText = first_text + second_text
+print(newText)
+
+print(int('30') + 1)
+
+int
+float
+str
+list
+dict
+set
+complex
+
+
+
+
+
+
+
